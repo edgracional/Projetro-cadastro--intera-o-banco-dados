1f3d7cf31dc74cd8da838c6e2c0c43e71f7d478c
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-18 14:50:45 -0300

    88. CSS Modularizado

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Estiloso.module.css'
+
+/* Criou uma função chamada estiloso */
+export default function Estiloso() {
+    
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando módulos</h1>
+        </div>
+    )
+}
