f18fde888adac56f38db2d62f2e5829eddc16b24
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-18 15:43:09 -0300

    89. Usando Componente

---
+++ src/pages/app.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../components/Cabecalho"
+
+export default function App() {
+    return (
+        <div>
+            <Cabecalho />
+            <Cabecalho />
+        </div>
+        
+    )
+}
