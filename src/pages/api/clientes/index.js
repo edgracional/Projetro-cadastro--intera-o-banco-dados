18ad0314dad9872a5b010bea11163d899d504232
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-21 14:50:14 -0300

    97. Usando API #01

---
+++ src/pages/api/clientes/index.js
@@ -0,0 +1,17 @@
+export default function handler(req, res){
+
+        if(req.method === "GET"){
+            handlerGet(req, res)
+        } else {
+            res.status(405).send()
+    }
+    
+}
+
+function handlerGet(req, res) {
+    res.status(200).json({
+        id: 3,
+        nome: 'Raqueli',
+        email: 'raqueli69@sexmail.com'
+    })
+}
